972c44fbaa5ef24b3b5a5c9027dd43583b1981cc
AntSwift1 <antswift10@gmail.com>
2024-07-07 14:41:24 -0400

    fix: changed android package name from starter code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.b07demosummer2024"
+    namespace = "com.tbb.taamcollection"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.b07demosummer2024"
+        applicationId = "com.tbb.taamcollection"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
