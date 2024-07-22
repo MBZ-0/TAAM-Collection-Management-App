90b9cfbe0f502a92df1a7f3a5dcef3ff6b007e37
jqlliu <2260216963liu@gmail.com>
2024-07-21 20:32:20 -0400

    Added GUI changes and created a login and logout skeleton

--- app/src/main/java/com/tbb/taamcollection/HomeFragment.java
+++ app/src/main/java/com/tbb/taamcollection/HomeFragment.java
@@ -17,6 +17,28 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
+
+    //REPLACE WITH SOMETHING ELSE
+    boolean loggedIn = false;
+    public void onLogin(Button back, Button add, Button remove, Button report, Button admin){
+        System.out.println("LOGIN");
+        loggedIn = true;
+        back.setVisibility(View.VISIBLE);
+        add.setVisibility(View.VISIBLE);
+        remove.setVisibility(View.VISIBLE);
+        report.setVisibility(View.VISIBLE);
+        admin.setVisibility(View.INVISIBLE);
+    }
+    public void onLogout(Button back, Button add, Button remove, Button report, Button admin){
+        System.out.println("LOGOUT");
+        loggedIn = false;
+        back.setVisibility(View.INVISIBLE);
+        add.setVisibility(View.INVISIBLE);
+        remove.setVisibility(View.INVISIBLE);
+        report.setVisibility(View.INVISIBLE);
+        admin.setVisibility(View.VISIBLE);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -26,7 +48,20 @@ public class HomeFragment extends Fragment {
         Button buttonScroller = view.findViewById(R.id.buttonScroller);
         Button buttonSpinner = view.findViewById(R.id.buttonSpinner);
         Button buttonManageItems = view.findViewById(R.id.buttonManageItems);
-        Button buttonAdmin = view.findViewById(R.id.admin); // Add this line
+        //CREATE NEW BUTTONS
+        Button buttonAdmin = view.findViewById(R.id.admin);
+        Button buttonBack = view.findViewById(R.id.adminBack);
+        Button buttonAdd = view.findViewById(R.id.add);
+        Button buttonRemove = view.findViewById(R.id.remove);
+        Button buttonReport = view.findViewById(R.id.report);
+        Button buttonView = view.findViewById(R.id.view);
+        Button buttonSearch = view.findViewById(R.id.search);
+
+        if(loggedIn){
+            onLogout(buttonBack, buttonAdd, buttonRemove, buttonReport, buttonAdmin);
+        }else {
+            onLogout(buttonBack, buttonAdd, buttonRemove, buttonReport, buttonAdmin);
+        }
 
         buttonRecyclerView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,7 +94,58 @@ public class HomeFragment extends Fragment {
         buttonAdmin.setOnClickListener(new View.OnClickListener() { // Add this block
             @Override
             public void onClick(View v) {
+                //TODO: COMMENT OUT following line
+                onLogin(buttonBack, buttonAdd, buttonRemove, buttonReport, buttonAdmin);
                 loadFragment(new LoginFragment()); // Replace with your AdminFragment
+            }
+        });
+        buttonAdd.setOnClickListener(new View.OnClickListener() { // Add this block
+            @Override
+            public void onClick(View v) {
+                if(loggedIn) {
+                    //TODO: Replace with ADD fragment
+                    //loadFragment(new LoginFragment());
+                }
+            }
+        });
+        buttonRemove.setOnClickListener(new View.OnClickListener() { // Add this block
+            @Override
+            public void onClick(View v) {
+                if(loggedIn) {
+                    //TODO: Replace with REMOVE fragment
+                    //loadFragment(new LoginFragment());
+                }
+            }
+        });
+        buttonReport.setOnClickListener(new View.OnClickListener() { // Add this block
+            @Override
+            public void onClick(View v) {
+                if(loggedIn) {
+                    //TODO: Replace with REPORT fragment
+                    //loadFragment(new LoginFragment());
+                }
+            }
+        });
+        buttonBack.setOnClickListener(new View.OnClickListener() { // Add this block
+            @Override
+            public void onClick(View v) {
+                if(loggedIn) {
+                    onLogout(buttonBack, buttonAdd, buttonRemove, buttonReport, buttonAdmin);
+                }
+            }
+        });
+        buttonSearch.setOnClickListener(new View.OnClickListener() { // Add this block
+            @Override
+            public void onClick(View v) {
+                //TODO: Replace with SEARCH fragment
+                //loadFragment(new LoginFragment());
+            }
+        });
+        buttonView.setOnClickListener(new View.OnClickListener() { // Add this block
+            @Override
+            public void onClick(View v) {
+                //TODO: Replace with VIEW fragment
+                //loadFragment(new LoginFragment());
             }
         });
 
