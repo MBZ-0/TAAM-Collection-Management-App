f3560d95db62f84458e51a96fe7649186e0805a1
AntSwift1 <antswift10@gmail.com>
2024-07-25 00:01:29 -0400

    feat: made changes for storage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.firebase.database)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/tbb/taamcollection/StorageHandler.java
+++ app/src/main/java/com/tbb/taamcollection/StorageHandler.java
@@ -1,4 +1,11 @@
 package com.tbb.taamcollection;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
 public class StorageHandler {
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private StorageReference storageRef = storage.getReference();}
 }
