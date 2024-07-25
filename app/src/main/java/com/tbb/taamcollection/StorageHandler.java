373824470c538f3316885f60ff97f7849256052d
AntSwift1 <antswift10@gmail.com>
2024-07-25 00:08:25 -0400

    fix: removed legacy import

--- app/src/main/java/com/tbb/taamcollection/StorageHandler.java
+++ app/src/main/java/com/tbb/taamcollection/StorageHandler.java
@@ -3,8 +3,6 @@ package com.tbb.taamcollection;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import java.util.concurrent.atomic.AtomicMarkableReference;
-
 public class StorageHandler {
     private FirebaseStorage storage = FirebaseStorage.getInstance("https://taam-collection-management-app-default-rtdb.firebaseio.com/");
     private StorageReference storageRef = storage.getReference();
