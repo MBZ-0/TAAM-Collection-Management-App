691ba782f5dcef7b6de5cf26afcaf7d85d8a016f
AntSwift1 <antswift10@gmail.com>
2024-07-25 00:10:17 -0400

    fix: moved some statements into a constructor

--- app/src/main/java/com/tbb/taamcollection/StorageHandler.java
+++ app/src/main/java/com/tbb/taamcollection/StorageHandler.java
@@ -1,11 +1,15 @@
 package com.tbb.taamcollection;
 
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 public class StorageHandler {
-    private FirebaseStorage storage = FirebaseStorage.getInstance("https://taam-collection-management-app-default-rtdb.firebaseio.com/");
-    private StorageReference storageRef = storage.getReference();
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
 
-
+    StorageHandler(String name){
+        storage = FirebaseStorage.getInstance("https://taam-collection-management-app-default-rtdb.firebaseio.com/");
+        storageRef = storage.getReference();
+    }
 }
