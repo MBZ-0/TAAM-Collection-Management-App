b1f45842bb39b69263797174d6d097dcd871530c
AntSwift1 <antswift10@gmail.com>
2024-07-27 19:23:32 -0400

    feat: made back button work

--- app/src/main/java/com/tbb/taamcollection/RemoveItemFragment.java
+++ app/src/main/java/com/tbb/taamcollection/RemoveItemFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,7 +37,7 @@ public class RemoveItemFragment extends Fragment {
         buttonCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                back();
+                loadFragment(new HomeFragment());
             }
         });
 
@@ -46,7 +48,10 @@ public class RemoveItemFragment extends Fragment {
         //ToDo - Implement
     }
 
-    private void back() {
-        //ToDo - Implement
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
